ced5fbed3919d6359ec420d720f3d90f9c4a90ea
gavazquez <gavazquez@gmail.com>
2020-03-25 19:52:23 +0100

    Fix for default settings

--- GazeTrackerCore/Settings/DetectionSettings.cs
+++ GazeTrackerCore/Settings/DetectionSettings.cs
@@ -2,9 +2,9 @@
 {
     public static class DetectionSettings
     {
-        public static volatile bool CalculateEyes = true;
-        public static volatile bool CalculateGazeLines = true;
-        public static volatile bool CalculateLandmarks = true;
+        public static volatile bool CalculateEyes = false;
+        public static volatile bool CalculateGazeLines = false;
+        public static volatile bool CalculateLandmarks = false;
         public static volatile bool CalculateBox = true;
     }
 }
